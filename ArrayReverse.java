0c0a4ce667587fc3b3d7f22fd3394c12f11c2875
YourBoB <zhaojunbao123@163.com>
2016-11-18 10:05:12 +1300

    Array reverse and datatype

---
+++ ArrayReverse.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        
+        for( int k=arr.length;k>0;k--){
+            System.out.print(arr[k-1]+" ");
+        }
+    }
+}
